cc4954da8c79e3aabd92a9243643dce79f2b3974
dmarie777 <deimarie777@gmail.com>
2024-01-18 22:31:57 -0500

    Added the binary Search algorithm

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,15 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let lo =0;
+    let hi =haystack.length;
+    do {
+        let m = lo + Math.floor((hi-lo)/2);
+        if (haystack[m] === needle) {
+            return true;
+        } else if (haystack[m] > needle) {
+            hi = m;
+        } else {
+            lo = m+1;
+        }
+    } while (lo < hi)
+    return false;
 }
