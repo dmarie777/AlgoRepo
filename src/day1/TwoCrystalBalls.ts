ab49c9502599066c3b0f1f8be57894f7e63011ec
dmarie777 <deimarie777@gmail.com>
2024-01-26 14:33:51 -0500

    Added solutions for the Two crystal ball problem

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,33 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jmpAmount = Math.floor(Math.sqrt(breaks.length));
 
+    let i = jmpAmount;
+    for (; i<breaks.length; i += jmpAmount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    i -= jmpAmount;
+    for (let j=0; j<jmpAmount && i<breaks.length; ++j, ++i) {
+        if(breaks[i]) {
+            return i;
+        }
+    }
+    return -1;
+
+//  Solution using do while___///////////////////////////
+    // const jump = Math.floor(Math.sqrt(breaks.length));
+    // let hi=0;
+    // do {
+    //     if (breaks[hi]) {
+    //         hi -= 1;
+    //         if (!breaks[hi]) {
+    //             return hi+1;
+    //         }
+    //     } else {
+    //         hi +=  jump;
+    //     }
+    // } while(hi < breaks.length)
+    // return -1;
 }
